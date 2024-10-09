2b6ae04c1f5070b3261eb6e47dcd3e8ee524a434
MDaemon Technologies, Ltd. <jonathan.ehman@mdaemon.com>
2024-10-09 15:00:50 -0500

    Update to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,9 @@
 import typescript from "@rollup/plugin-typescript"
 import commonjs from "@rollup/plugin-commonjs"
 import terser from "@rollup/plugin-terser"
-import pkg from "./package.json" assert { type: "json" }
+import * as fs from "fs"
+
+const pkg = JSON.parse(fs.readFileSync("package.json", "utf8"));
 
 export default [
   {
